65ce60ef75f18cce29f86cbbc5da814c2de8a418
ryanyogan <ryanyogan@gmail.com>
2020-01-04 08:51:07 -0800

    Return 200 on OK responses, return 404 on Not Found responses, cleaned up tests

--- server.go
+++ server.go
@@ -21,7 +21,14 @@ type PlayerServer struct {
 // ServeHTTP will take a request, and return a response via the `ResponseWriteer`
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	fmt.Fprint(w, p.store.GetPlayerScore(player))
+
+	score := p.store.GetPlayerScore(player)
+
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, score)
 }
 
 // GetPlayerScore takes a player name and returns their score as a string.
--- server_test.go
+++ server_test.go
@@ -7,6 +7,16 @@ import (
 	"testing"
 )
 
+// Stub / Spy setup
+type StubPlayerStore struct {
+	scores map[string]int
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
+
 func TestGETPlayers(t *testing.T) {
 	store := StubPlayerStore{
 		map[string]int{
@@ -18,31 +28,38 @@ func TestGETPlayers(t *testing.T) {
 	server := &PlayerServer{&store}
 
 	t.Run("returns Ryan's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Ryan", nil)
+		request := newGetScoreRequest("Ryan")
 		response := httptest.NewRecorder()
 		server.ServeHTTP(response, request)
 
+		assertStatusCode(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "20")
 	})
 
 	t.Run("returns Floyd's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		request := newGetScoreRequest("Floyd")
 		response := httptest.NewRecorder()
 		server.ServeHTTP(response, request)
 
+		assertStatusCode(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "10")
+	})
+
+	t.Run("returns 404 on missing player", func(t *testing.T) {
+		request := newGetScoreRequest("Apollo")
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, request)
+
+		got := response.Code
+		want := http.StatusNotFound
+
+		if got != want {
+			t.Errorf("got status code %d, wanted a status code of %d", got, want)
+		}
 	})
 }
 
-type StubPlayerStore struct {
-	scores map[string]int
-}
-
-func (s *StubPlayerStore) GetPlayerScore(name string) int {
-	score := s.scores[name]
-	return score
-}
-
+// Helper Assertion Functions
 func newGetScoreRequest(name string) *http.Request {
 	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
 	return req
@@ -52,5 +69,12 @@ func assertResponseBody(t *testing.T, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("response body is wrong, recieved %q, and wanted %q", got, want)
+	}
+}
+
+func assertStatusCode(t *testing.T, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("did not get the correct HTTP status code, received %d and wanted %d", got, want)
 	}
 }
