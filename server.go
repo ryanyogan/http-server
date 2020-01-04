7fa3f9a0d4169ad2043203c4b5bc682f191d6790
ryanyogan <ryanyogan@gmail.com>
2020-01-04 09:02:36 -0800

    Split out actions based on GET or POST

--- server.go
+++ server.go
@@ -20,11 +20,15 @@ type PlayerServer struct {
 
 // ServeHTTP will take a request, and return a response via the `ResponseWriteer`
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusAccepted)
-		return
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w)
+	case http.MethodGet:
+		p.showScore(w, r)
 	}
+}
 
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 	score := p.store.GetPlayerScore(player)
 
@@ -33,6 +37,10 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
 }
 
 // GetPlayerScore takes a player name and returns their score as a string.
