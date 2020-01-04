fc7e6be5d043d97e9af09e26761c3b4851a5e005
ryanyogan <ryanyogan@gmail.com>
2020-01-04 08:12:17 -0800

    DRY'd up the tests

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,12 +14,7 @@ func TestGETPlayers(t *testing.T) {
 
 		PlayerServer(response, request)
 
-		got := response.Body.String()
-		want := "20"
-
-		if got != want {
-			t.Errorf("got %q, but wanted %q", got, want)
-		}
+		assertResponseBody(t, response.Body.String(), "20")
 	})
 
 	t.Run("returns Floyd's score", func(t *testing.T) {
@@ -27,11 +23,18 @@ func TestGETPlayers(t *testing.T) {
 
 		PlayerServer(response, request)
 
-		got := response.Body.String()
-		want := "10"
-
-		if got != want {
-			t.Errorf("got %q, but wanted %q", got, want)
-		}
+		assertResponseBody(t, response.Body.String(), "10")
 	})
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func assertResponseBody(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, recieved %q, and wanted %q", got, want)
+	}
 }
